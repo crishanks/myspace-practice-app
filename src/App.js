f6eeee786f44f71c089d8ca0611fb66344b8cc36
github username <crishanks12@gmail.com>
2019-01-24 15:45:37 -0800

    added background image and text component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import VerticalNavigation from './Components/vertical-navigation';
+import Jumbotron from './Components/jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <VerticalNavigation />
+        <Jumbotron />
       </div>
     );
   }
---
+++ src/Components/jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Jumbotron extends React.Component {
+    render() {
+        return (
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>Myspace is BACK boyz</h1>
+                    <h3>It's Lit.</h3>
+                    <button>Yeet Yeet!</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Jumbotron;
