782cece8ab0770aaf40d38139456c8e61eac3e03
github username <crishanks12@gmail.com>
2019-01-23 18:02:56 -0800

    imported icon images, added css, completed vertical navigation component

--- src/Components/vertical-navigation.js
+++ src/Components/vertical-navigation.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import MySpaceLogo from '../images/myspace-logo.png';
+import SearchIcon from '../images/search-icon.png';
+import FlameIcon from '../images/flame.png';
+import MusicIcon from '../images/music.png';
+import PeopleIcon from '../images/people.png';
+import VideoIcon from '../images/video.png';
+
 
 class VerticalNavigation extends React.Component {
     render() {
         return (
             <div className="sidebar">
-                <VerticalNavigation>
                     <div id="sidebar-content">
                         <div>
                             <a id="search" href="http://google.com">
-                                <img id="myspace-logo" src="./src/images/myspace-logo.png" alt="myspace logo"/>
+                                <img id="myspace-logo" src={MySpaceLogo} alt="myspace logo"/>
                             </a>
                             <a href="http://google.com">
-                                <img src="./src/images/search-icon.png" alt=""/>
+                                <img src={SearchIcon} alt="search icon"/>
                                 <span>Search</span>
                             </a>
                             <h2>DISCOVER</h2>
@@ -19,28 +25,28 @@ class VerticalNavigation extends React.Component {
                                 <ul>
                                     <li>
                                         <a href="http://google.com">
-                                            <img src="./src/images/flame.png" alt="flame"/>
+                                            <img src={FlameIcon} alt="flame"/>
                                             <span>Featured</span>
                                         </a>
                                     </li>
                                     
                                     <li>
                                         <a href="http://google.com">
-                                            <img src="./src/images/music.png" alt="music"/>
+                                            <img src={MusicIcon} alt="music"/>
                                             <span>Music</span>
                                         </a>
                                     </li>
 
                                     <li>
                                         <a href="http://google.com">
-                                            <img src="./src/images/video.png" alt="video"/>
+                                            <img src={VideoIcon} alt="video"/>
                                             <span>Videos</span>
                                         </a>
                                     </li>
 
                                     <li>
                                         <a href="http://google.com">
-                                            <img src="./src/images/people.png" alt="people"/>
+                                            <img src={PeopleIcon} alt="people"/>
                                             <span>People</span>
                                         </a>
                                     </li>
@@ -58,7 +64,6 @@ class VerticalNavigation extends React.Component {
                     <div>
                         <p>Disclaimer: This site is too dope for weenies. We will not be held responsible for any damage recieved upon viewing and/or interacting with our website including but not limited to: epileptic seizures, regretful tattoos and unwanted pregnancies.</p>
                     </div>
-                </VerticalNavigation>
             </div>
         );
     }
